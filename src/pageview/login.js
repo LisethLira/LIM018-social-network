062dbbe3af1ea9d87d8981af372123b030196b43
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-07-03 13:56:40 -0500

    Avances en css

--- src/pageview/login.js
+++ src/pageview/login.js
@@ -1,22 +1,28 @@
 export default () => {
-  const viewLogin = `<section>
-  <form id="login">
+  const viewLogin = ` <header class="nameLogo">
+  <img class="gatitoLogo" src="GATITO LOGO.png">
+  <h1>PUUR LOVE</h1>
+  <h2>Una comunidad hecha para los amantes de los gatos.</h2>
+   <a href="#/login"></a>
+</header>
+  <section class="secLogin">
+  <form class="formLogin" id="login">
     <legend>Inicia sesión</legend>
     <label>Correo electrónico:</label>
     <input id="emailLogin">
     <label>Contraseña:</label>
     <input id="passwordLogin">
-    <button id="btnLogin">Inicia Sesión</button>
+    <button class="btnLogin" id="btnLogin">Inicia Sesión</button>
     <legend>o</legend>
   </form>
   <div>
-    <button id="btnLoginGoogle">
+    <button class="btnLoginGoogle" id="btnLoginGoogle">
       <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAgRJREFUWEftltsxREEQhv+NABEgAyJABIgAGRABIkAEyMBGgAyIABkQAfVVzVYdx/Rl9titfdiu2tqHM5dvuv++jLRgNlowHi2BoogM9dCupG9JW5I+Jb1L+pL0El1sfZ8G6EDSkSRgVo2DAXuSdFkg03wtQACcF5D0BZLuJJ0VD4b7skDHkm7D0+wFhPOweM09JgPECwnREENXeDjUVgQ01DM8Ig3DYg+IFz06bvko+uDV/AgL2YboechKK0wERJbsGEA3ki4coZJ9fCfcYZi6d1ge8kKFOB+GCMrbawFx4X5lI545nRWMFzKqb99eS6aglZlZzUOWmKm66MIyvtUeYq1/rtWlFqC9oLC1wABZDX8NiJfSIvq2FpT/ViA8RDR+WQvQZtAoZwZkaei/Q1bVZIuGIlFTSC2rFdg0EIeS2pT+rjHjbGfHiM7GDUlvFdJqgZ1HYbSSpHq3BeQ11pPSozLF0TpnXJrwnzO8bu81V1oLYF7VpsVQPmpjrpkgHhCjBFB9LU1ehaYAo5vTVriY1McrjCDsr5npHRbPY0DrQjGsIXLTsxEQh+H6q4xggjWpyTEDxD3MR9dO+CJepkvCGA5rWSAuxNVMgNYUaUHdFy+nxpYWoMmFiBaP8b9uUCByEgKvIv60TQPUPZxM6qc1AE0Q3QOHAqVfnl24BIo8tXAe+gFPeGwlzWWFWwAAAABJRU5ErkJggg=="/>
       Inicia sesión con Google</button>
   </div>
-  <div>
+  <div class="ntcRegis">
     <p>¿No tienes cuenta?</p>
-    <a href="#/Registrate">Registrate</a>
+    <a class="hrefRegistrate" href="#/Registrate">Registrate</a>
   </div>
 </section>`;
 
