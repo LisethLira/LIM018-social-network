6b5abdc22c176633ae91e55dc0110dbcd7ac6b75
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-07-09 01:05:24 -0500

    Vista Home 2

--- src/pageview/home.js
+++ src/pageview/home.js
@@ -6,36 +6,37 @@ export default () => {
         <h1>PUUR LOVE</h1>
         <ul>
             <li>
-            <a href="#/home">
-                <img src="image/home.png">
-            </a>
+                <a href="#/home">
+                    <img src="image/home.png">
+                </a>
             </li>
             <li>
-            <a href="#/perfil">
-                <img src="image/perfil.png">
-            </a>
+                <a href="#/perfil">
+                    <img src="image/perfil.png">
+                </a>
             </li>
             <li>
-            <img src="image/buscar.png">
-            <input type="search" placeholder="Buscar">
+                <img src="image/buscar.png">
+                <input type="search" placeholder="Buscar">
             </li>
             <li>
-            <a href="#/salir">Salir</a>
+                <a href="#/salir">Salir</a>
             </li>
         </ul>
+        <img src="image/menu.png">
         </nav>
     </header>
     <section class= "secHome">
-    <input class= "agregarPublicacion" placeholder="Agrega una publicación:">
-    <button class="btnPublicar">Publicar</button>
-    <div>
-        <label class="NameUser">Nombre de usuario</label> 
-        <img src="image/tresPuntos.png">
-        <label class="publicacion">Publicación</label> 
-        <img src="image/like.png">
-        <label class="contadorLikes">"N°"</label>
-        <button class="btnComentar">Comentar</button>
-        </div>
+        <input class= "agregarPublicacion" placeholder="Agrega una publicación:">
+        <button class="btnPublicar">Publicar</button>
+        <div>
+            <label class="NameUser">Nombre de usuario</label> 
+            <img src="image/tresPuntos.png">
+            <label class="publicacion">Publicación</label> 
+            <img src="image/like.png">
+            <label class="contadorLikes">"N°"</label>
+            <button class="btnComentar">Comentar</button>
+            </div>
     </section>
     </section>`;
 
