c4615fe526fe7381c2f16d6da92d2482841b88ed
LisethLira <liseth.lira.123@gmail.com>
2022-07-21 18:46:43 -0500

    cambios antes de seletePost

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,3 @@
 // aqui exportaras las funciones que necesites
-/* import login from '../pageview/login.js'; */
-// export const likeCounter = (counter) => {
-//   let counter = + counter; 
-//   return counter;
-// } 
+
 
