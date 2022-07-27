3e2d10e42d2b9eaf0fbd2c3d71a709f5dca62e43
GabrielaC3 <Gabycr.3.2.gcr@gmail.com>
2022-07-27 09:33:49 -0500

    probando salida imagen

--- src/firebase/baseDatos.js
+++ src/firebase/baseDatos.js
@@ -1,14 +1,16 @@
-import { getFirestore, 
-    doc, 
-    addDoc, 
-    collection, 
-    setDoc, 
-    getDocs, 
-    onSnapshot, 
-    deleteDoc, 
-    getDoc, 
+import {
+    getFirestore,
+    doc,
+    addDoc,
+    collection,
+    setDoc,
+    getDocs,
+    onSnapshot,
+    deleteDoc,
+    getDoc,
     updateDoc,
-app, } from './firebaseConfig.js';
+    app,
+} from './firebaseConfig.js';
 
 // Initialize firestore
 const db = getFirestore(app);
@@ -16,52 +18,62 @@ const db = getFirestore(app);
 // Base de datos de registro de usuarios
 export const createUserRegisterDB = (uid, name, email, password) => {
     setDoc(doc(db, 'users', uid), {
-      name,
-      email,
-      password,
+        name,
+        email,
+        password,
     });
-  };
-  
-export const savePost = async(nameUser, fecha, newpost, uid, image) => {
+};
+
+export const savePost = async (nameUser, fecha, newpost, uid, image) => {
     try {
-        const cratePost = await addDoc(collection(db, 'posts'), {
-        nameUser,
-        fecha,
-        newpost,
-        uid,
-        image
-    });
-    console.log("post publicado");
+        if (image) {
+            const cratePost = await addDoc(collection(db, 'posts'), {
+                nameUser,
+                fecha,
+                newpost,
+                uid,
+                image
+            });
+        } else {
+            const cratePost = await addDoc(collection(db, 'posts'), {
+                nameUser,
+                fecha,
+                newpost,
+                uid,
+            });
+        }
+
+        console.log("post publicado");
     }
     catch (e) {
-    console.error("Error adding posts: ", e);
+        console.error("Error adding posts: ", e);
     }
 };
 
-export const getPost = async() => { 
+export const getPost = async () => {
     const querySnapshot = await getDocs(collection(db, "posts"));
     return querySnapshot;
-    };
-    
-export const getUser = async() => { 
+};
+
+export const getUser = async () => {
     const userInfoPost = await getDocs(collection(db, "users"));
     return userInfoPost;
-    };
+};
 
 export const getUserById = (userId, colection) => {
     const docRef = doc(db, colection, userId);
     const docSnap = getDoc(docRef).then((userDoc) => userDoc.data());
     return docSnap;
-    };
+};
 
 
-export const onGetPost = async(callback) => {
-    const currentPost = await onSnapshot(collection(db, "posts"), 
-    (callback))
+export const onGetPost = async (callback) => {
+    const currentPost = await onSnapshot(collection(db, "posts"),
+        (callback))
 };
 
 export const deletePost = id => deleteDoc(doc(db, 'posts', id));
 
 export const gettingPost = id => getDoc(doc(db, 'posts', id));
 
-export const editPost = (id, changePost) => updateDoc(doc(db, 'posts', id), { "newpost" : changePost });
+export const editPost = (id, changePost) => updateDoc(doc(db, 'posts', id), { "newpost": changePost });
